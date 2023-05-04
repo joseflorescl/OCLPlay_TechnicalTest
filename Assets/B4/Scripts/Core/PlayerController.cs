7135270a925f3bb4aa9d1c4716a0a0925b00d84f
joseflorescl <joseflores.cl@gmail.com>
2023-05-03 22:58:32 -0400

    Mecanim animation added to Player: rotation in Z axis

--- Assets/B4/Scripts/Core/PlayerController.cs
+++ Assets/B4/Scripts/Core/PlayerController.cs
@@ -10,6 +10,14 @@ public class PlayerController : MonoBehaviour
 
     SkeletonAnimation arloAnimation;
     Spine.AnimationState spineAnimationState;
+    Animator anim;
+    int grabHash;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        grabHash = Animator.StringToHash("Grab");
+    }
 
     private void Start()
     {
@@ -31,8 +39,12 @@ public class PlayerController : MonoBehaviour
 
     private void GrabButtonPressedHandler()
     {
+        anim.SetTrigger(grabHash);
         spineAnimationState.SetAnimation(0, cop, false);
         spineAnimationState.AddAnimation(0, swinging, true, 0);
+
+        // TODO: vfx de las partículas al agarrar un tesoro
+        // TODO: size adecuado del player, ahora está muy grande
     }
     
 }
